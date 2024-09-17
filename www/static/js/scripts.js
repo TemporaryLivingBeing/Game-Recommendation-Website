a8da47a9e21a9fac7031a3192d10641a4fb939fa
PoliticianSeal <politicianseal@gmail.com>
2024-09-17 15:04:24 -0400

    Hamburger menu functionality fully implemented. Probably.

--- www/static/js/scripts.js
+++ www/static/js/scripts.js
@@ -4,3 +4,12 @@ function toggleDarkMode() {
 }
 
 document.addEventListener("DOMContentLoaded", autoSwitchMode);
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
