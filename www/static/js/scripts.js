6b9710c0f351b72637e12957bd9a27919a7934df
PoliticianSeal <politicianseal@gmail.com>
2024-10-29 11:06:59 -0400

    Recommendations page, backend for game recommendation, and a bunch of CSS changes

--- www/static/js/scripts.js
+++ www/static/js/scripts.js
@@ -1,4 +1,12 @@
 function toggleDarkMode() {
+    let i = document.getElementById("sunMoon");
+    
+    if (i.classList.contains('fa-sun')) {
+        i.classList.replace('fa-sun', 'fa-moon');
+    } else {
+        i.classList.replace('fa-moon', 'fa-sun');
+    }
+
     let element = document.body;
     element.classList.toggle("dark");
 
@@ -22,14 +30,49 @@ function hamburger() {
       }
   }
 
-function openPop() {
-    const popDialogue =
+  function openPop() {             /* popup https://www.geeksforgeeks.org/how-to-open-a-popup-on-click-using-javascript/-->> */
+
+    const popupDialogue =
         document.getElementById(
             "popupDialogue"
         );
-    popDialogue.style.visibility =
-        popDialogue.style.visibility ===
+    popupDialogue.style.visibility =
+        popupDialogue.style.visibility ===
             "visible"
             ? "hidden"
-            : "visible";
+            : "visible"
+
+}
+
+let slideIndex = 1;
+window.onload = function(){
+    showSlides(slideIndex);
+}
+
+//gotten fromhttps://www.w3schools.com/howto/howto_js_slideshow.asp
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
