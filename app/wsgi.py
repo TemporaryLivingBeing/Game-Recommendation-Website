7bdf4037caca546f723a7d6532fa0d2cd1567b00
PoliticianSeal <politicianseal@gmail.com>
2024-11-05 15:26:22 -0500

    Contact and Recommendations page fixed with wsgi adjusted

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,9 +1,9 @@
 from contact import create_app as create_contact_app
-# from app.recommendations import create_app as create_rec_app
+from recommendations import create_rec_app
 
-application = create_contact_app()  # For contact form
-# rec = create_rec_app()
+application = create_contact_app()
+rec = create_rec_app()
 
 if __name__ == "__main__":
     application.run()
-    # rec.run()
+    rec.run()
